8cf89c53248aa403f779ff5a1ae7637c22e20bb4
Pavel Pleshkov <pleshkov.p@gmail.com>
2025-06-10 09:32:15 +0300

    feat: add Hooks useRef, useEffect

--- src/components/pages/AboutPage/AboutPage.jsx
+++ src/components/pages/AboutPage/AboutPage.jsx
@@ -1,9 +1,55 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+
 import s from "./AboutPage.module.css";
 
+function VideoPlayer({ src, isPlaying }) {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (isPlaying) {
+      console.log("playing");
+      videoRef.current.play();
+    } else {
+      console.log("pause");
+      videoRef.current.pause();
+    }
+  }, [isPlaying]);
+
+  return (
+    <video
+      style={{ width: 200 + "px", display: "block", margin: "10px auto" }}
+      ref={videoRef}
+      src={src}
+      loop
+      playsInline
+    />
+  );
+}
+
 function AboutPage() {
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <section className={s.aboutPage}>
       <h4>About</h4>
+      <div>
+        <h5>useEffect()</h5>
+        <button
+          type="button"
+          onClick={() => {
+            setIsPlaying((prev) => !prev);
+          }}
+          style={{ padding: "10px" }}
+        >
+          {isPlaying ? "Pause" : "Play"}
+        </button>
+        <VideoPlayer
+          src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+          isPlaying={isPlaying}
+        />
+      </div>
     </section>
   );
 }
--- src/components/pages/FormPage/FormPage.jsx
+++ src/components/pages/FormPage/FormPage.jsx
@@ -1,8 +1,28 @@
 import { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
 import s from "./FormPage.module.css";
 
 function Form() {
   const [value, setValue] = useState("");
+
+  const ref = useRef("");
+  const domRef = useRef(null);
+  // console.log(ref.current);
+
+  useEffect(() => {
+    console.log("log from useEffect after rendering");
+  });
+
+  function handleClick() {
+    ref.current = +(ref.current + 1);
+    console.log(ref.current);
+  }
+
+  function handleClickRef() {
+    domRef.current.style.color = "crimson";
+    console.log(domRef.current.innerHTML);
+  }
 
   return (
     <>
@@ -18,8 +38,45 @@ function Form() {
       </form>
       <p>Typed text:</p>
       <pre>{value}</pre>
+
+      <button type="button" onClick={handleClick}>
+        Click for ref++
+      </button>
+      <div>
+        Ref don't triggers rerender, ref appears/updates on the screen only when
+        typing above:
+        <br />
+        {ref.current}
+      </div>
+      <div ref={domRef}>
+        DIV with <i>ref</i>
+      </div>
+      <button type="button" onClick={handleClickRef}>
+        Show DIV.innerHTML in console and color DIV
+      </button>
     </>
   );
 }
+
+// function Form() {
+//   const [value, setValue] = useState("");
+
+//   return (
+//     <>
+//       <form className={s.form}>
+//         <label htmlFor="input">Type text:</label>
+//         <input
+//           id="input"
+//           type="text"
+//           value={value}
+//           onChange={(e) => setValue(e.target.value)}
+//           placeholder="Enter text"
+//         />
+//       </form>
+//       <p>Typed text:</p>
+//       <pre>{value}</pre>
+//     </>
+//   );
+// }
 
 export default Form;
